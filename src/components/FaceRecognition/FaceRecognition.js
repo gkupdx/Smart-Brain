7881809a3f8379d10b27862d6c3a420813d8f90c
Grady Ku <gkupdx@gmail.com>
2022-10-05 16:12:00 -0700

    Fix CSS import; minor refactors

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import './FaceRecognition';
+import './FaceRecognition.css';
 
 // Component that utilizes the Clarifai API to detect faces in images
 const FaceRecognition = ({ box, imageUrl }) => {
+
     return (
         <div className='center ma'>
             <div className='absolute mt2'>
                 <img id='inputimage' src={imageUrl} alt="Clarifai" width="500px" height="auto" />
-                <div className='bounding-box' style={{ top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol }}></div>
+                <div className='bounding-box' style={{ top: box.top, right: box.right, bottom: box.bottom, left: box.left }}></div>
             </div>
         </div>
     )
